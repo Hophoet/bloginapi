df6d396d4e14a6170f2f6a0ba7a4314876ae0175
hophoet <hophoet@gmail.com>
2020-10-15 10:16:14 +0000

    (urls) - set user profile getting root url

--- core/urls.py
+++ core/urls.py
@@ -9,12 +9,14 @@ urlpatterns = [
     path('post/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/<int:post_id>/comments/', views.PostCommentsView.as_view(), name='post_comments'),
     path('add-new-post/', views.AddNewPostView.as_view(), name='add_new_post'),
-    path('post/<int:post_id>/add-comment/', views.AddCommentToPostView.as_view(), name='add_comment_to_post'),
+    path('post/<int:post_id>/add-comment/', views.AddCommentToPostView.as_view(),
+        name='add_comment_to_post'
+    ),
     path('toggle-post-like/', views.TogglePostLikeView.as_view(), name='toggle_post_like'),
     path('toggle-comment-like/', views.ToggleCommentLikeView.as_view(), name='toggle_comment_like'),
     path('edit-post/', views.PostEditView.as_view(), name='post-edit'),
     path('categories/', views.CategoryListView.as_view(), name='categories'),
     path('delete-post/', views.PostDeleteView.as_view(), name='post-delete'),
     path('post-is-liked/', views.PostIsLikedByUser.as_view(), name='post-is-liked'),
-    
+    path('user-profile/', views.UserProfile.as_view(), name='user-profile')
 ]
