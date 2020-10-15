d80613753704519f9a6b6ac425ca44914d7236ba
hophoet <hophoet@gmail.com>
2020-10-15 14:31:12 +0000

    (urls) - update add comment management root url, remove post id argument from the url

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('post/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/<int:post_id>/comments/', views.PostCommentsView.as_view(), name='post_comments'),
     path('add-new-post/', views.AddNewPostView.as_view(), name='add_new_post'),
-    path('post/<int:post_id>/add-comment/', views.AddCommentToPostView.as_view(),
+    path('post/add-comment/', views.AddCommentToPostView.as_view(),
         name='add_comment_to_post'
     ),
     path('toggle-post-like/', views.TogglePostLikeView.as_view(), name='toggle_post_like'),
