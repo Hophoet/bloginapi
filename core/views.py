4984cf762ade2a3a4cf730b50e53632fe76f74d0
hophoet <hophoet@gmail.com>
2020-11-27 23:52:23 +0000

    add posts listing view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+#rest framework
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+#models
+from .models import (Post, Comment)
+#serializers
+from .serializers import (PostSerializer, CommentSerializer)
 
-# Create your views here.
+
+class PostListView(APIView):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+        posts_serializer = PostSerializer(posts, many=True)
+        return Response(posts_serializer.data)
