0a7b59e93631c1822c8b860650662212b5e31e42
hophoet <hophoet@gmail.com>
2020-11-27 23:51:17 +0000

    add core models (Post, Category, Comment, LikePost, CommentLike)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+#user model
+User = get_user_model()
+
+#Post category
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    image = models.CharField(max_length=500)
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+
+class PostLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.username
+
+class CommentLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.user.username
