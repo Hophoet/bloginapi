f8269f0d4a0dc7a6651d2bd681ef8ff8c14456f6
hophoet <hophoet@gmail.com>
2020-11-27 23:54:48 +0000

    add core app root url to the project main root urls

--- bloginapi/urls.py
+++ bloginapi/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
+
+    path('', include('core.urls')),
 ]
