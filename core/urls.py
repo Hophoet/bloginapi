e81c5697545f0f7cbe7cffc4cdfb48673c6674b3
hophoet <hophoet@gmail.com>
2020-11-28 11:03:48 +0000

    add the post detail endpoint url pattern to the core app url patterns

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
-    path('posts/', views.PostListView.as_view(), name='posts')
+    path('posts/', views.PostListView.as_view(), name='posts'),
+    path('post/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail')
 ]
