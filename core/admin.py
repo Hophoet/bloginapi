1dd023268f43f5f506c81f85283ca183594502f6
hophoet <hophoet@gmail.com>
2020-11-28 16:10:40 +0000

    add Models(PostLike, CommentLike) to django admin site

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import (Post, Comment, Category)
+from .models import (Post, Comment, Category, PostLike, CommentLike)
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Category)
+admin.site.register(PostLike)
+admin.site.register(CommentLike)
