65ddb86358c50410545e6a6c69bc2130c520df6e
hophoet <hophoet@gmail.com>
2020-11-28 15:45:40 +0000

    add urls (add_new_post, add_comment_to_post, toggle_post_like)

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('posts/', views.PostListView.as_view(), name='posts'),
     path('post/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/<int:post_id>/comments/', views.PostCommentsView.as_view(), name='post_comments'),
+    path('add-new-post/', views.AddNewPostView.as_view(), name='add_new_post'),
+    path('post/<int:post_id>/add-comment/', views.AddCommentToPostView.as_view(), name='add_comment_to_post'),
+    path('like-post/', views.TogglePostLikeView.as_view(), name='toggle_post_like'),
 ]
