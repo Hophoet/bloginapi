1e40c23a64c2d8446eb57dbee8212de33cf99825
hophoet <hophoet@gmail.com>
2020-11-28 12:21:50 +0000

    add PostCommentsView view root url to the core app urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ app_name = "core"
 
 urlpatterns = [
     path('posts/', views.PostListView.as_view(), name='posts'),
-    path('post/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail')
+    path('post/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:post_id>/comments/', views.PostCommentsView.as_view(), name='post_comments'),
 ]
