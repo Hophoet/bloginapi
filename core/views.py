81330d72f99f9d2cfc931750f9c54202ae93a810
hophoet <hophoet@gmail.com>
2020-11-28 12:20:59 +0000

    add PostCommentsView(endpoint) for post comments getting

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 #rest framework
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_404_NOT_FOUND
 from rest_framework.response import Response
 from rest_framework.views import APIView
 #models
@@ -22,3 +24,17 @@ class PostDetailView(APIView):
         post = get_object_or_404(Post, id=post_id)
         post_serializer = PostSerializer(post)
         return Response(post_serializer.data)
+
+class PostCommentsView(APIView):
+    def get(self, request, *args, **kwargs): 
+        try:
+            post_id = kwargs.get('post_id')
+            post = Post.objects.get(pk=post_id)
+            comments = Comment.objects.filter(post=post_id).order_by('-timestamp')
+            post_serializer = PostSerializer(post)
+            comments_serializer = CommentSerializer(comments, many=True)
+        except Exception as error:
+            return Response({'detail': f'{error}'}, status=HTTP_404_NOT_FOUND)
+            # raise NotFound
+        else:
+            return Response(comments_serializer.data)
