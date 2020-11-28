7abcdaacf7552c4f8d1f28edb7284d17f60104dd
hophoet <hophoet@gmail.com>
2020-11-28 15:44:59 +0000

    add views (AddCommentToPostView, AddNewPostView, TogglePostLikeView)

--- core/views.py
+++ core/views.py
@@ -7,9 +7,9 @@ from rest_framework.status import HTTP_404_NOT_FOUND
 from rest_framework.response import Response
 from rest_framework.views import APIView
 #models
-from .models import (Post, Comment)
+from .models import (Post, Comment, PostLike)
 #serializers
-from .serializers import (PostSerializer, CommentSerializer)
+from .serializers import (PostSerializer, CommentSerializer, PostLikeSerializer)
 
 
 class PostListView(APIView):
@@ -26,6 +26,7 @@ class PostDetailView(APIView):
         return Response(post_serializer.data)
 
 class PostCommentsView(APIView):
+    
     def get(self, request, *args, **kwargs): 
         try:
             post_id = kwargs.get('post_id')
@@ -38,3 +39,72 @@ class PostCommentsView(APIView):
             # raise NotFound
         else:
             return Response(comments_serializer.data)
+
+
+class AddNewPostView(APIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = PostSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                            context={'request':request})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        # raise NotFound
+
+
+class AddCommentToPostView(APIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CommentSerializer
+    def post(self, request, *args, **kwargs):
+        post_id = kwargs.get('post_id')
+        post = get_object_or_404(Post, pk=post_id)
+        user = request.user
+        content = request.data.get('content')
+        data = {
+            'content':content,
+            'user':user.pk,
+            'post':post.pk
+        }
+        comment_serializer = self.serializer_class(data=data,
+                                                    context={'request':request})
+        if comment_serializer.is_valid(raise_exception=True):
+            
+            comment = comment_serializer.save()
+            print('COMMENT', comment)
+            # print(post.comment_set.set)
+            # print(request.data)
+            post.comment_set.set((comment,))
+            return Response(comment_serializer.data)
+
+class TogglePostLikeView(APIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = PostLikeSerializer
+    def post(self, request, *args, **kwargs):
+        user = request.data.get('user')
+        post = request.data.get('post')
+        post_like_serializer = self.serializer_class(data=request.data, context={'request':request})
+        if post_like_serializer.is_valid(raise_exception=True):
+            post_likes = PostLike.objects.filter(user=user, post=post)
+            # print(dir(post_likes), post_likes.count())
+            if post_likes.exists():
+                print('liked')
+                post_like = post_likes.first()
+                post_like.delete()
+                return Response(
+                    {
+                        'state': 'post disliked',
+                        'likes': post_likes.count(),
+                        'user': request.user.username
+                    }
+                    , status=200)
+
+            post_like_serializer.save()
+            return Response(
+                {
+                    'state': 'post liked',
+                    'likes': post_likes.count(),
+                    'user': request.user.username
+                }
+                , status=200)
+        
