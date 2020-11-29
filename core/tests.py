c4766b2b48d67d6c0545b80625389d042a3b614b
hophoet <hophoet@gmail.com>
2020-11-29 11:28:33 +0000

    (test)- add post listing view test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+from rest_framework.test import APITestCase
+from rest_framework.permissions import IsAuthenticated 
 
-# Create your tests here.
+from .models import (Post, Comment, Category, User)
+# Home page
+class PostListTestCase(APITestCase):
+    #test that home page returns a 200
+    def test_post_list_view_returns_200(self):
+        response = self.client.get(reverse('core:posts'))
+        self.assertEqual(response.status_code, 200)
+
