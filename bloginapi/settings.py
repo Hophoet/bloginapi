c8661c1cf964386d66900d269fc3294ea0102caf
hophoet <hophoet@gmail.com>
2020-12-01 16:05:42 +0000

    (settings) - add django-heroku package settings

--- bloginapi/settings.py
+++ bloginapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication'
     ]
 }
+
+django_heroku.settings(locals())
