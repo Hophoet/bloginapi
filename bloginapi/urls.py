4fcb8d9078ec8ac8ccf5f27758dbb500fdf5516d
hophoet <hophoet@gmail.com>
2020-12-23 12:13:41 +0000

    (urls) - set rest-auth registration root url

--- bloginapi/urls.py
+++ bloginapi/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
 
     path('', include('core.urls')),
 ]
